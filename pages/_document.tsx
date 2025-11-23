112124391f04e929217846957fe55eb632f99c71
Max Rohde <{ID}+{username}@users.noreply.github.com>
2025-11-23 12:55:53 +0700

    Improving styling

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,6 +20,10 @@ export default class MyDocument extends Document {
               href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap"
               rel="stylesheet"
             />
+            <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+            rel="stylesheet"
+          ></link>
           </Head>
 
           <body>
