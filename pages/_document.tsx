2fd2f433c19f456c659a36be55f658322b168e3d
Max Rohde <{ID}+{username}@users.noreply.github.com>
2025-11-24 10:26:31 +0700

    Fixing favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,31 +9,29 @@ export default class MyDocument extends Document {
           <Head>
             <script async src="https://www.googletagmanager.com/gtag/js?id=G-4FW6GQDFK1"></script>
             <script
-            dangerouslySetInnerHTML={{
-              __html: `window.dataLayer = window.dataLayer || [];
+              dangerouslySetInnerHTML={{
+                __html: `window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-4FW6GQDFK1');`,
-            }}
-          ></script>
-            <link rel='shortcut icon' href='/favicon.ico' />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='32x32'
-              href='favicon.png'
-            />
+              }}
+            ></script>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+            <link rel="manifest" href="/site.webmanifest" />
 
-            <link rel='manifest' href='/manifest.json' />
             <link
               href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap"
               rel="stylesheet"
             />
             <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-            rel="stylesheet"
-          ></link>
+              href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+              rel="stylesheet"
+            ></link>
           </Head>
 
           <body>
